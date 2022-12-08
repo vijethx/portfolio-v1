5973d6001fd577c18a1a8d149e199014fc76d338
Vijeth (GitHub Desktop) <38877972+vijethx@users.noreply.github.com>
2022-12-08 08:12:09 +0530

    placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,78 @@
-import Head from "next/head";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import TechStack from "../components/TechStack";
-import Projects from "../components/Projects";
 import Exploring from "../components/Exploring";
+import Footer from "../components/Footer";
+import Head from "next/head";
+import Link from "next/link";
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
+import TechStack from "../components/TechStack";
 
 export default function Home() {
-  return (
-    <div className='m-10'>
-      <Head>
-        <title>Vijeth ☀</title>
-        {/* Primary Meta Tags */}
-        <title>Vijeth ☀</title>
-        <meta name='title' content='Vijeth ☀' />
-        <meta
-          name='description'
-          content="Vijeth's real estate on the internet to showoff his buidls"
-        />
+	return (
+		<div className="m-10">
+			<Head>
+				<title>Vijeth ☀</title>
+				{/* Primary Meta Tags */}
+				<title>Vijeth ☀</title>
+				<meta
+					name="title"
+					content="Vijeth ☀"
+				/>
+				<meta
+					name="description"
+					content="Vijeth's real estate on the internet to showoff his buidls"
+				/>
 
-        {/* Open Graph / Facebook */}
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://vijeth.vercel.app/' />
-        <meta property='og:title' content='Vijeth ☀' />
-        <meta
-          property='og:description'
-          content="Vijeth's real estate on the internet to showoff his buidls"
-        />
-        <meta
-          property='og:image'
-          content='https://vijeth.vercel.app/luffy.jpg'
-        />
+				{/* Open Graph / Facebook */}
+				<meta
+					property="og:type"
+					content="website"
+				/>
+				<meta
+					property="og:url"
+					content="https://vijeth.vercel.app/"
+				/>
+				<meta
+					property="og:title"
+					content="Vijeth ☀"
+				/>
+				<meta
+					property="og:description"
+					content="Vijeth's real estate on the internet to showoff his buidls"
+				/>
+				<meta
+					property="og:image"
+					content="https://vijeth.vercel.app/luffy.jpg"
+				/>
 
-        {/* Twitter */}
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:url' content='https://vijeth.vercel.app/' />
-        <meta property='twitter:title' content='Vijeth ☀' />
-        <meta
-          property='twitter:description'
-          content="Vijeth's real estate on the internet to showoff his buidls"
-        />
-        <meta
-          property='twitter:image'
-          content='https://vijeth.vercel.app/luffy.jpg'
-        />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+				{/* Twitter */}
+				<meta
+					property="twitter:card"
+					content="summary_large_image"
+				/>
+				<meta
+					property="twitter:url"
+					content="https://vijeth.vercel.app/"
+				/>
+				<meta
+					property="twitter:title"
+					content="Vijeth ☀"
+				/>
+				<meta
+					property="twitter:description"
+					content="Vijeth's real estate on the internet to showoff his buidls"
+				/>
+				<meta
+					property="twitter:image"
+					content="https://vijeth.vercel.app/luffy.jpg"
+				/>
+				<link
+					rel="icon"
+					href="/favicon.ico"
+				/>
+			</Head>
 
-      <main className='font-[Satoshi] container flex flex-col max-w-xl mx-auto'>
-        <Navbar />
+			<main className="font-[Satoshi] container flex flex-col max-w-xl mx-auto">
+				{/* <Navbar />
         <div className='pb-5'>
           <h1 className='text-4xl md:text-5xl font-bold pb-5 flex'>
             gm! i&apos;m vijeth 👋
@@ -87,10 +112,19 @@ export default function Home() {
 
         <TechStack />
         <Projects />
-        <Exploring />
-      </main>
+        <Exploring /> */}
+				<p>getting redesigned. brb </p>
+				<p>
+					till then, find me on{" "}
+					<a
+						className="font-bold"
+						href={"https://twitter.com/vijethx"}>
+						twitter
+					</a>
+				</p>
+			</main>
 
-      <Footer />
-    </div>
-  );
+			{/* <Footer /> */}
+		</div>
+	);
 }
