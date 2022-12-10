e35d7956764506c56319edd52bd5f9624faa8e58
Vijeth (GitHub Desktop) <38877972+vijethx@users.noreply.github.com>
2022-12-10 11:35:33 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -72,48 +72,48 @@ export default function Home() {
 			</Head>
 
 			<main className="font-[Satoshi] container flex flex-col max-w-xl mx-auto">
-				{/* <Navbar />
-        <div className='pb-5'>
-          <h1 className='text-4xl md:text-5xl font-bold pb-5 flex'>
-            gm! i&apos;m vijeth 👋
-          </h1>
-          <p className='text-gray-700 text-lg py-2'>
-            Inquizitive self-taught developer / buildooor with a keen interest
-            on the web and EVM-based blockchains.
-          </p>
-          <div className='text-gray-700 text-lg py-2 flex flex-col space-y-2'>
-            <p className='font-bold underline underline-offset-2'>
-              What am I doing currently?
-            </p>
-            <p className=''>
-              <span className=''>🎯</span> Shipping (atleast) one project a
-              month till Dec &apos;22
-            </p>
-            <p className=''>
-              <span className=''>🚀</span> Focusing on getting better at writing
-              robust smart contracts, building super-fast web apps and shipping
-              products
-            </p>
-          </div>
-          <p className='text-gray-700 text-lg py-2'>
-            If you&apos;re building something cool, I&apos;d love to hear about
-            it. Even if you&apos;re not and just want to hang out and chill,
-            i&apos;m pretty much active on{" "}
-            <a
-              href='http://twitter.com/vijethx'
-              target='_blank'
-              className='text-sky-600 underline underline-offset-2 font-bold'
-              rel='noopener noreferrer'>
-              twitter
-            </a>
-            .
-          </p>
-        </div>
+				<Navbar />
+				<div className="pb-5">
+					<h1 className="text-4xl md:text-5xl font-bold pb-5 flex">
+						gm! i&apos;m vijeth 👋
+					</h1>
+					<p className="text-gray-700 text-lg py-2">
+						Inquizitive self-taught developer / buildooor with a
+						keen interest on the web and EVM-based blockchains.
+					</p>
+					<div className="text-gray-700 text-lg py-2 flex flex-col space-y-2">
+						<p className="font-bold underline underline-offset-2">
+							What am I doing currently?
+						</p>
+						<p className="">
+							<span className="">🎯</span> Shipping (atleast) one
+							project a month till Dec &apos;22
+						</p>
+						<p className="">
+							<span className="">🚀</span> Focusing on getting
+							better at writing robust smart contracts, building
+							super-fast web apps and shipping products
+						</p>
+					</div>
+					<p className="text-gray-700 text-lg py-2">
+						If you&apos;re building something cool, I&apos;d love to
+						hear about it. Even if you&apos;re not and just want to
+						hang out and chill, i&apos;m pretty much active on{" "}
+						<a
+							href="http://twitter.com/vijethx"
+							target="_blank"
+							className="text-sky-600 underline underline-offset-2 font-bold"
+							rel="noopener noreferrer">
+							twitter
+						</a>
+						.
+					</p>
+				</div>
 
-        <TechStack />
-        <Projects />
-        <Exploring /> */}
-				<p>getting redesigned. brb </p>
+				<TechStack />
+				<Projects />
+				<Exploring />
+				{/* <p>getting redesigned. brb </p>
 				<p>
 					till then, find me on{" "}
 					<a
@@ -121,10 +121,10 @@ export default function Home() {
 						href={"https://twitter.com/vijethx"}>
 						twitter
 					</a>
-				</p>
+				</p> */}
 			</main>
 
-			{/* <Footer /> */}
+			<Footer />
 		</div>
 	);
 }
